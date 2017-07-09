bb9c103cab755cceba6595cd638871ac81fc6800
Keshav <rorprjin@gmail.com>
2017-07-09 19:35:33 +0000

    Casestatement, breakstatement

---
+++ breakstmt.rb
@@ -0,0 +1,9 @@
+for i in 1..5 
+
+if ( i >2 ) 
+    break
+end
+
+puts "Value of is is #{i}"
+
+end
---
+++ casestatement.rb
@@ -0,0 +1,20 @@
+$age = 57
+case $age
+    when 1 .. 10
+        puts "With in the range of 10"
+    when 11 .. 20
+        puts "With in the range of 11 and 20"
+    when 21 .. 30
+        puts "With in the range of 21 and 30"
+    when 31 .. 40
+        puts "With in the range of 31 and 40"
+    when 41 .. 50
+        puts "With in the range of 41 and 50"
+    when 51 .. 60
+        puts "With in the range of 51 and 60"
+    when 61 .. 70
+        puts "With in the range of 61 and5 70"
+    
+else
+    puts " Else section "
+end
