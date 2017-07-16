e1f76a2a8178b175fd13eedcfb1d9048ec024a3a
Keshav <rorprjin@gmail.com>
2017-07-16 01:47:16 +0000

    Displayingthedatetimecomponents

---
+++ componentsofdateandtime.rb
@@ -0,0 +1,12 @@
+time = Time.new
+puts "current time is :" + time.inspect
+puts time.year
+puts time.month
+puts time.day
+puts time.wday
+puts time.yday
+puts time.hour
+puts time.min
+puts time.sec
+puts time.usec
+puts time.zone
