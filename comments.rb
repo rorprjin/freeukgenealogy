0b8ef078e5c8047d295c790c868d9878a58d35ee
Keshav <rorprjin@gmail.com>
2017-07-16 01:46:38 +0000

    Commenting codes

---
+++ comments.rb
@@ -0,0 +1,16 @@
+=begin
+This is a multiline comment and con spwan as many lines as you
+like. But =begin and =end should come in the first line only. 
+=end
+# This is a single line comment.
+# This is a comment.
+# This is a comment, too.
+# This is a comment, too.
+# I said that already.
+=begin
+This is a comment.
+This is a comment, too.
+This is a comment, too.
+I said that already.
+=end
+puts "This file teaches how to comment ruby source code. Open  the source file."
